a1fbdc1b5a42d34b6bae439655648751f88a02c7
michaelStillwell <michael.stillwell@hotmail.com>
2022-06-28 16:40:19 -0500

    implemented e-to-digit

--- e-to-digit/src/main.rs
+++ e-to-digit/src/main.rs
@@ -1,3 +1,15 @@
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What digit:");
+    let to = std::io::stdin()
+        .lock()
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+
+    println!("Result: {:.1$}", std::f64::consts::E, to);
 }
